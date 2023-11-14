a3eced72e52d3aea019d4b8904fad512157ab81c
abolfazl <abolfazl>
2023-11-14 15:17:18 +0330

    feat: add multiple skills

--- src/configs/skillsSet.js
+++ src/configs/skillsSet.js
@@ -1,3 +1,12 @@
-const skillSets = [{ name: "HTML", id: 1 }];
+const skillSets = [
+  { name: "HTML", id: 1 },
+  { name: "CSS", id: 2 },
+  { name: "JS", id: 3 },
+  { name: "React", id: 4 },
+  { name: "Vue", id: 5 },
+  { name: "TS", id: 6 },
+  { name: "Tailwind", id: 7 },
+  { name: "Bootstrap", id: 8 },
+];
 
 export default skillSets;
